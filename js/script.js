15e8b3e86f4f2c52be7bfff466d370f8dbf1b833
Tomas Liubertas <tomas.liubertas@gmail.com>
2021-01-31 10:00:58 +0200

    edited code according tasak requirements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
-const randMsg = {
-    _greeting: ["Whaddup", "Sup", "What's crackin'", "Peek-a-boo", "Howdy", "Hello", "Ahoy", "Hi", "What's cookin, good lookin", "Aloha","Hola", "Que pasa", "Ciao"],
-    _reference: ["Beauty", "Darling", "Dawg", "Amigo", "Esse", "My friend", "Homeslice", "Matey"],
-    _pickUpLine: ["Are you an exam? Because I have been studying you like crazy.", "Are you a haunted house? I’m going to scream when I’m in you.", "Are you an elevator? Because I’ll go up and down on you.", "Are you a campfire? Because you’re hot and I want s’more.", "my homework? Cause I’m not doing you but I definitely should be.", "Are you a drill sergeant? Because you have my privates standing at attention.", "Are you a supermarket sample? Because I want to taste you again and again without any sense of shame.", "Are you a sea lion? Because I can sea you lion in my bed tonight.", "Are you Dracula? You looked a little thirsty when you were looking at me."],
-    get greeting() {
-        return this._greeting[Math.floor(Math.random() * this._greeting.length)];    
-    },
-    get reference() {
-        return this._reference[Math.floor(Math.random() * this._reference.length)];    
-    },
-    get pickUpLine() {
-        return this._pickUpLine[Math.floor(Math.random() * this._pickUpLine.length)];    
-    },
-    finalMessage() {
-        document.getElementById("demo").innerHTML = this.greeting + ' ' + this.reference + '! ' + this.pickUpLine;
-    }
+const dataArr = {
+    greeting: ["Whaddup", "Sup", "What's crackin'", "Peek-a-boo", "Howdy", "Hello", "Ahoy", "Hi", "What's cookin, good lookin", "Aloha","Hola", "Que pasa", "Ciao"],
+    reference: ["Beauty", "Darling", "Dawg", "Amigo", "Esse", "My friend", "Homeslice", "Matey"],
+    pickUpLine: ["Are you an exam? Because I have been studying you like crazy.", "Are you a haunted house? I’m going to scream when I’m in you.", "Are you an elevator? Because I’ll go up and down on you.", "Are you a campfire? Because you’re hot and I want s’more.", "my homework? Cause I’m not doing you but I definitely should be.", "Are you a drill sergeant? Because you have my privates standing at attention.", "Are you a supermarket sample? Because I want to taste you again and again without any sense of shame.", "Are you a sea lion? Because I can sea you lion in my bed tonight.", "Are you Dracula? You looked a little thirsty when you were looking at me."]
+}
 
+function randNum (num) {
+    return Math.floor(Math.random() * num);
 };
+
+let message = [];
+
+for (let prop in dataArr) {
+    let index = randNum(dataArr[prop].length)
+
+    switch (prop) {
+        case 'greeting':
+            message.push(`Try greeting your friend with '${dataArr[prop][index]}' next time you chat !`)
+            break
+        case 'reference':
+            message.push(`Next time you will be at the bar, try calling someone by '${dataArr[prop][index]}'!`)
+            break
+        case 'pickUpLine':
+            message.push(`Here is a pick up line for the next time you will go out: '${dataArr[prop][index]}'!`)
+            break
+        default:
+            message.push(`Something went wrong :(`)
+    }
+}
+
+const finalMessage = () => {
+    document.getElementById("demo").innerHTML = message;
+}
